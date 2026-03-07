253f3b9d1b0f7803c80073096c9388b56cb20c49
Keshav Datta <keshavdatta23@gmail.com>
2026-03-07 17:40:06 +0530

    Add CF 43 A.cpp

---
+++ Sessions/CF 43 A.cpp
@@ -0,0 +1,30 @@
+// Football problem
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<string, int> goals;
+    string team;
+
+    for (int i = 0; i < n; i++) {
+        cin >> team;
+        goals[team]++;
+    }
+
+    string winner;
+    int maxGoals = 0;
+
+    for (auto it : goals) {
+        if (it.second > maxGoals) {
+            maxGoals = it.second;
+            winner = it.first;
+        }
+    }
+
+    cout << winner;
+
+    return 0;
+}
