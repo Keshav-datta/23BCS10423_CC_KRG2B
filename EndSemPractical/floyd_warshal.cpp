fa339af06a07835f3b9db2ef9814b6ec3dfcb7a5
Keshav Datta <keshavdatta23@gmail.com>
2026-04-27 10:40:31 +0530

    Implement Floyd-Warshall algorithm for shortest paths

---
+++ EndSemPractical/floyd_warshal.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> dist(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> dist[i][j];
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (dist[i][k] != 1e9 && dist[k][j] != 1e9)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
